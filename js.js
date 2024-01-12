bef167a399bfaecae3b6ee53497c3be5ff4d9222
Jamie <16787928+shme14@users.noreply.github.com>
2024-01-12 16:57:34 -0700

    finished the project

--- js.js
+++ js.js
@@ -1,7 +1,11 @@
 
 
 const sketchContainerDiv = document.querySelector('#sketchContainer');
+const squareSizeInput = document.querySelector('#squareSizeInput');
+const squareSizeInputBtn = document.querySelector('#squareSizeInputBtn');
 
+var colors = ['red','blue','green','yellow'];
+var random_color = colors[Math.floor(Math.random() * colors.length)];
 let squareSize = '20px';
 let xGrid = 16;
 let yGrid = 16;
@@ -13,19 +17,32 @@ function createGrid(xGrid, yGrid) {
         gridRowDiv.classList.add('gridRow');
         for(let ii = 0; ii<xGrid; ii++){
             const gridSquareDiv = document.createElement('div');
-            var colors = ['red','blue','green','yellow'];
-            var random_color = colors[Math.floor(Math.random() * colors.length)];
-            gridSquareDiv.style.backgroundColor = random_color;
+            gridSquareDiv.style.backgroundColor = 'Black';
             gridSquareDiv.id = String(i) + "," + String(ii);
             gridSquareDiv.classList.add('gridSquare');
-            // gridSquareDiv.textContent = ('X')
-            gridSquareDiv.style.width = squareSize;
-            gridSquareDiv.style.height = squareSize;
-
-
-
+            gridSquareDiv.style.flex = "1";
+            gridSquareDiv.style.aspectRatio = "1 / 1";
+            gridSquareDiv.addEventListener('mouseover', () => {
+                random_color = colors[Math.floor(Math.random() * colors.length)];
+                gridSquareDiv.style.backgroundColor = random_color;
+            });
             gridRowDiv.appendChild(gridSquareDiv);
         }
         sketchContainerDiv.appendChild(gridRowDiv);
     }
 }
+
+squareSizeInputBtn.addEventListener('click', () => {
+    let NewSize = squareSizeInput.value;
+    if ((0 < NewSize) && (NewSize < 101) ) {
+        while (sketchContainerDiv.firstChild) {
+            sketchContainerDiv.removeChild(sketchContainerDiv.firstChild);
+        }
+        xGrid = NewSize;
+        yGrid = NewSize;
+        createGrid(xGrid,yGrid)
+    }
+    else{
+        alert("Must be between 1 and 100")
+    }
+});
